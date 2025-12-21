6d05e2842e437ba9a693b132a7caf3a090248250
Krimi mohamed ali <mohamedali.krimi@esprit.tn>
2025-12-21 17:33:53 +0100

    project creation

---
+++ src/main/java/com/example/reservi/ReserviApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reservi/ReserviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
